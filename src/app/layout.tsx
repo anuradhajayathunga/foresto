466de7a19edb49df2bd7c6614db5fbf10f3f90be
Anuradha <ishiwaraanuradha@gmail.com>
2026-01-03 16:06:33 +0530

    feat: added sinhala font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,32 +1,41 @@
-import type { Metadata } from 'next';
-import { Poppins } from 'next/font/google';
-import './globals.css';
-import { Providers } from './providers';
-import type { PropsWithChildren } from 'react';
-import { Toaster } from 'react-hot-toast';
-import { cn } from '@/lib/utils';
-import RootLoading from './loading';
+import type { Metadata } from 'next'
+import { Poppins, Noto_Sans_Sinhala } from 'next/font/google'
+import './globals.css'
+import { Providers } from './providers'
+import type { PropsWithChildren } from 'react'
+import { Toaster } from 'react-hot-toast'
+import { cn } from '@/lib/utils'
+import RootLoading from './loading'
 
 const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["300", "400", "500", "600", "700"],
-  variable: "--font-poppins",
-});
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+  variable: '--font-poppins',
+})
+
+const sinhala = Noto_Sans_Sinhala({
+  subsets: ['sinhala'],
+  weight: ['400', '500', '600', '700'],
+  variable: '--font-sinhala',
+})
 
 export const metadata: Metadata = {
   title: 'Foresto | Modern and Elegant Restaurant Experience.',
   description: 'Foresto — A modern and elegant restaurant experience.',
-};
+}
 
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
-   <html lang="en" suppressHydrationWarning className={poppins.variable}>
-  <body className={cn(poppins.className)}>
-    <RootLoading /> 
-    <Providers>{children}</Providers>
-    <Toaster position="bottom-right" />
-  </body>
-</html>
-
-  );
+    <html
+      lang="si"
+      suppressHydrationWarning
+      className={cn(poppins.variable, sinhala.variable)}
+    >
+      <body className={cn(poppins.className)}>
+        <RootLoading />
+        <Providers>{children}</Providers>
+        <Toaster position="bottom-right" />
+      </body>
+    </html>
+  )
 }
